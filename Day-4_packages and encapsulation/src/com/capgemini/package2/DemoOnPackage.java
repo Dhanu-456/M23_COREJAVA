f9d9ffcf31fa2ba69355965e1246b8a7e72cbc15
lenovo <lenovo@Lenovo>
2022-08-15 14:44:16 +0530

    First Commit

---
+++ Day-4_packages and encapsulation/src/com/capgemini/package2/DemoOnPackage.java
@@ -0,0 +1,13 @@
+package com.capgemini.package2;
+import com.capgemini.package1.packages;
+
+public class DemoOnPackage 
+{
+	public static void main(String[] args) {
+		packages p = new packages ();
+		p.display(11, "Shraddha");
+		
+		
+	}
+	
+}
