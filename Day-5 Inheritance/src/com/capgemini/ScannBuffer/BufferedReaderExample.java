e38e81df1b93c6bb47af68ef4b1343760d7e42da
lenovo <lenovo@Lenovo>
2022-08-15 15:01:51 +0530

    Second commit

---
+++ Day-5 Inheritance/src/com/capgemini/ScannBuffer/BufferedReaderExample.java
@@ -0,0 +1,17 @@
+package com.capgemini.ScannBuffer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BufferedReaderExample {
+
+	public static void main(String[] args)throws IOException  {
+		BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = b.readLine();
+		System.out.print(str);
+	}
+
+}
---
+++ Day-5 Inheritance/src/com/capgemini/ScannBuffer/ScannerClass.java
@@ -0,0 +1,15 @@
+package com.capgemini.ScannBuffer;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		//String str = s.nextLine();
+		int a = s.nextInt();
+		System.out.println(a);
+		
+	}
+
+}
