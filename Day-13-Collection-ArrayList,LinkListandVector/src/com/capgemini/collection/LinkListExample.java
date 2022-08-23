83c7aae01fad7cedb3e06563a0182de79ea8115d
Dhanu-456 <110384383+Dhanu-456@users.noreply.github.com>
2022-08-23 11:49:26 +0530

    second commit

--- Day-13-Collection-ArrayList,LinkListandVector/src/com/capgemini/collection/LinkListExample.java
+++ Day-13-Collection-ArrayList,LinkListandVector/src/com/capgemini/collection/LinkListExample.java
@@ -1,10 +1,33 @@
 package com.capgemini.collection;
 
-public class LinkListExample {
+import java.util.LinkedList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+public class LinkListExample 
+{
+@SuppressWarnings("unchecked")
+	public static void main(String[] args)
+	{
+	@SuppressWarnings("rawtypes")
+	List obj=new LinkedList<>();
+	obj.add(11);
+	obj.add("Dhanshri");
+	obj.add(76.90);
+	obj.add(11);
+	obj.add(76);
+	System.out.println(obj);
+	System.out.println(obj.size());
+	obj.remove(0);
+	System.out.println(obj.lastIndexOf(11));
+	System.out.println(obj);
+	}
+
+
+
+
+	
 
 	}
 
-}
+
